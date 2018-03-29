7530e978ea437e852dc9b6c5ff3df872bf564f1c
Maxence POUTORD <maxence.poutord@gmail.com>
2018-03-29 14:54:32 +0100

    link login/logout with vuex

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
   <div id="app">
-    <p v-if="currentUser.displayName">Hello {{ currentUser.displayName }}!</p>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
 export default {
-  computed: {
-    ...mapGetters([
-      'currentUser'
-    ])
-  },
   created () {
     this.$store.dispatch('loadLoggedInUser')
   }
