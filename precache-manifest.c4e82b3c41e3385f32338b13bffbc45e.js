0c0e95b3c21df4b222e1e306bdf27599abb6e972
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-23 05:02:38 +0000

    Deploy maxpou/dictionary-game to github.com/maxpou/dictionary-game.git:gh-pages

---
+++ precache-manifest.c4e82b3c41e3385f32338b13bffbc45e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc38215961aab4270f2",
+    "url": "/dictionary-game/css/app.c454d805.css"
+  },
+  {
+    "revision": "1e5a1b0873825a79a398",
+    "url": "/dictionary-game/css/dashboard.567d38e2.css"
+  },
+  {
+    "revision": "8b6fe50e9926a374367f",
+    "url": "/dictionary-game/css/game.24c41742.css"
+  },
+  {
+    "revision": "9ee63fba6d9bf6fb4e79",
+    "url": "/dictionary-game/css/login.11294f60.css"
+  },
+  {
+    "revision": "e901b7fd736d0639aaeba214b8077973",
+    "url": "/dictionary-game/img/all-screens.e901b7fd.png"
+  },
+  {
+    "revision": "131eeb4456e687c2a5ba91f5efe45f14",
+    "url": "/dictionary-game/img/background-desktop.131eeb44.jpg"
+  },
+  {
+    "revision": "f6cf6bb179d3cf31b652af0184ddedbe",
+    "url": "/dictionary-game/img/background-mobile.f6cf6bb1.jpg"
+  },
+  {
+    "revision": "a398c1fb67eea712294764e313e8f470",
+    "url": "/dictionary-game/img/background-tablet.a398c1fb.jpg"
+  },
+  {
+    "revision": "9e801fec0442ac3a8baf20feb084fd0d",
+    "url": "/dictionary-game/img/brain.9e801fec.png"
+  },
+  {
+    "revision": "b029b5cc7bc930dd7193ba145324d6a5",
+    "url": "/dictionary-game/img/free.b029b5cc.png"
+  },
+  {
+    "revision": "b3727efcf8efddc5e04386e1dddf74dd",
+    "url": "/dictionary-game/index.html"
+  },
+  {
+    "revision": "8fbf372e1cb6ae198539",
+    "url": "/dictionary-game/js/admin.97437e6f.js"
+  },
+  {
+    "revision": "cdc38215961aab4270f2",
+    "url": "/dictionary-game/js/app.449b6083.js"
+  },
+  {
+    "revision": "46df3639084ddc3e1d29",
+    "url": "/dictionary-game/js/chunk-vendors.041d58af.js"
+  },
+  {
+    "revision": "1e5a1b0873825a79a398",
+    "url": "/dictionary-game/js/dashboard.102c0a2a.js"
+  },
+  {
+    "revision": "8b6fe50e9926a374367f",
+    "url": "/dictionary-game/js/game.bc6d2be9.js"
+  },
+  {
+    "revision": "9ee63fba6d9bf6fb4e79",
+    "url": "/dictionary-game/js/login.015b8dec.js"
+  },
+  {
+    "revision": "48a28a87e60343c516b4df960ed4060e",
+    "url": "/dictionary-game/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dictionary-game/robots.txt"
+  }
+]);
